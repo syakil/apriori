396d5978bb16a08e2b37c3f3df49d4da606530b5
syakil <syakilahmadginanjar@gmail.com>
2021-03-19 00:30:12 +0700

    lolos item

--- app/Http/Controllers/ProsesAprioriController.php
+++ app/Http/Controllers/ProsesAprioriController.php
@@ -17,6 +17,6 @@ class ProsesAprioriController extends Controller
         $clearitemset1= DB::select('TRUNCATE `itemset1`');
         $item_set1 = DB::select('INSERT INTO `itemset1`(`atribut`, `jumlah`) SELECT kode_produk,count(DISTINCT id_penjualan) FROM `penjualan_detail` GROUP BY kode_produk');
         $support_item1 = DB::select('UPDATE `itemset1` SET support = jumlah/'.$jumlah_transaksi .'*100,lolos = if(support > jumlah,1,0) ');
-
+        $lolos_item1 = DB::select('UPDATE `itemset1` SET lolos = if(support > jumlah,1,0) ');
     }
 }
