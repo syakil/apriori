6a40a0d1aec8f6327897dfff0efb455ecd4238e1
syakil <syakilahmadginanjar@gmail.com>
2021-03-19 00:31:31 +0700

    min

--- app/Http/Controllers/ProsesAprioriController.php
+++ app/Http/Controllers/ProsesAprioriController.php
@@ -10,13 +10,13 @@ class ProsesAprioriController extends Controller
 {
     public function proses(){
 
-        $min = 30;
+        $min = 10;
         $transaksi = DB::select('SELECT count(DISTINCT id_penjualan) as jumlah FROM `penjualan_detail`');
         $jumlah_transaksi =  $transaksi[0]->jumlah;
 
         $clearitemset1= DB::select('TRUNCATE `itemset1`');
         $item_set1 = DB::select('INSERT INTO `itemset1`(`atribut`, `jumlah`) SELECT kode_produk,count(DISTINCT id_penjualan) FROM `penjualan_detail` GROUP BY kode_produk');
         $support_item1 = DB::select('UPDATE `itemset1` SET support = jumlah/'.$jumlah_transaksi .'*100,lolos = if(support > jumlah,1,0) ');
-        $lolos_item1 = DB::select('UPDATE `itemset1` SET lolos = if(support > jumlah,1,0) ');
+        $lolos_item1 = DB::select('UPDATE `itemset1` SET lolos = if(support >= '.$min.',1,0) ');
     }
 }
