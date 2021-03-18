55cb4c023b3d440759b5b700dfd06132699794a2
syakil <syakilahmadginanjar@gmail.com>
2021-03-19 00:20:16 +0700

    jumlah_transaksi

--- app/Http/Controllers/ProsesAprioriController.php
+++ app/Http/Controllers/ProsesAprioriController.php
@@ -11,8 +11,8 @@ class ProsesAprioriController extends Controller
     public function proses(){
 
         $min = 30;
-        $jumlah_transaksi = DB::select('SELECT count(DISTINCT id_penjualan) FROM `penjualan_detail`');
-        dd($jumlah_transaksi);
+        $transaksi = DB::select('SELECT count(DISTINCT id_penjualan) FROM `penjualan_detail`');
+        $jumlah_transaksi =  $transaksi[0];
 
         $clearitemset1= DB::select('TRUNCATE `itemset1`');
         $item_set1 = DB::select('INSERT INTO `itemset1`(`atribut`, `jumlah`) SELECT kode_produk,count(DISTINCT id_penjualan) FROM `penjualan_detail` GROUP BY kode_produk');
