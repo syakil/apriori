ec6d84f4f30c3ba3244376c7186ee8b4c3eacf79
syakil <syakilahmadginanjar@gmail.com>
2021-03-24 17:41:39 +0700

    confidence

--- app/Http/Controllers/ProsesAprioriController.php
+++ app/Http/Controllers/ProsesAprioriController.php
@@ -8,174 +8,15 @@ use DB;
 
 class ProsesAprioriController extends Controller{
 
-    public function pyroses(){
-      
-        $data_mentah = DB::select('SELECT id_penjualan, GROUP_CONCAT(CONCAT(kode_produk)) AS produk FROM penjualan_detail GROUP BY id_penjualan ASC');
-
-      
-
-        $data_penjualan = array();
-        foreach ($data_mentah as $key => $value) {
-            
-            $produk = explode(",",$value->produk);
-            $data_penjualan[] = $produk;
-
-        }
-
-        $jumlah_penjualan = count($data_penjualan);
-
-        $min = 10;
-        
-        $transaksi = DB::select('SELECT count(DISTINCT id_penjualan) as jumlah FROM `penjualan_detail`');
-        $jumlah_transaksi =  $transaksi[0]->jumlah;
-    
-        $clearitemset1= DB::select('TRUNCATE `itemset1`');
-        $clearitemset2= DB::select('TRUNCATE `itemset2`');
-        $clearitemset3= DB::select('TRUNCATE `itemset3`');
-    
-
-        $itemset12 = DB::select('INSERT INTO `itemset1`(`atribut`, `jumlah`) SELECT kode_produk,count(DISTINCT id_penjualan) FROM `penjualan_detail` GROUP BY kode_produk');
-
-        $support_item1 = DB::select('UPDATE `itemset1` SET support =round( jumlah/'.$jumlah_transaksi .'*100),lolos = if(support > jumlah,1,0) ');
-        $lolos_item1 = DB::select('UPDATE `itemset1` SET lolos = if(support >= '.$min.',1,0) ');
-
-        $item_set1 = DB::select('SELECT atribut FROM `itemset1` WHERE lolos = 1');
-        $jumlah_lolos_itemset1 = DB::select('SELECT count(atribut) as jumlah FROM `itemset1` WHERE lolos = 1');
-        $itemset2 = '';
-        $varian = [];
-
-        if ($jumlah_lolos_itemset1 > 0) {
-         
-            for ($i=0; $i < $jumlah_lolos_itemset1[0]->jumlah ; $i++) { 
-                
-                for ($h=0; $h < $jumlah_lolos_itemset1[0]->jumlah ; $h++) { 
-                    
-                    if ($item_set1[$i]->atribut != $item_set1[$h]->atribut) {
-                        
-                        $cekvarian1 = array('atribut1' => $item_set1[$i]->atribut,'atribut2' => $item_set1[$h]->atribut);
-                        $cekvarian2 = array('atribut1' => $item_set1[$h]->atribut,'atribut2' => $item_set1[$i]->atribut);
-
-                        if (!in_array($cekvarian1,$varian) && !in_array($cekvarian2,$varian)) {
-                            
-                            $varian[] = ['atribut1' => $item_set1[$i]->atribut,'atribut2' => $item_set1[$h]->atribut];
-
-                        }
-
-                    }
-                
-                }
-            }
-
-            $jumlahvarianitemset2 = count($varian);
-
-            $itemsetjumlah = array();
-
-            for ($j=0; $j < $jumlahvarianitemset2 ; $j++) { 
-                
-                $jumlahtransaksi2 = DB::select('SELECT count(DISTINCT id_penjualan) as jumlah FROM penjualan_detail WHERE kode_produk = '.$varian[$j]['atribut1'] . '
-                            AND id_penjualan IN(SELECT id_penjualan FROM penjualan_detail WHERE kode_produk = '.$varian[$j]['atribut2'].')');
-                
-                $itemsetjumlah[] = ['atribut1' => $varian[$j]['atribut1'],'atribut2' => $varian[$j]['atribut2'],'jumlah'=>$jumlahtransaksi2[0]->jumlah
-                ];
-                        
-            }
-
-            for ($j=0; $j < $jumlahvarianitemset2 ; $j++) { 
-                $itemset2 .= '('. $itemsetjumlah[$j]['atribut1'] .','.$itemsetjumlah[$j]['atribut2'].','.$itemsetjumlah[$j]['jumlah'].'),';
-            }
-
-            $itemset2 = substr($itemset2, 0, -1);
-
-            $insertitemset2 = DB::select('INSERT INTO itemset2 (atribut1,atribut2,jumlah) VALUES '.$itemset2);
-            $support_item2 = DB::select('UPDATE `itemset2` SET support =round( jumlah/'.$jumlah_transaksi .'*100)');
-            $lolos_item2 = DB::select('UPDATE `itemset2` SET lolos = if(support >= '.$min.',1,0) ');
-
-
-            $item_set2_atribut1 = DB::select('SELECT atribut FROM `itemset1` WHERE atribut IN (SELECT atribut1 FROM `itemset2` WHERE lolos = 1)');
-            $item_set2_atribut2 = DB::select('SELECT atribut FROM `itemset1` WHERE atribut IN (SELECT atribut2 FROM `itemset2` WHERE lolos = 1)');
-            $jumlah_lolos_itemset2_atribut1 = DB::select('SELECT count(atribut) as jumlah FROM `itemset1` WHERE atribut IN (SELECT atribut1 FROM `itemset2` WHERE lolos = 1)');
-            $jumlah_lolos_itemset2_atribut2 = DB::select('SELECT count(atribut) as jumlah FROM `itemset1` WHERE atribut IN (SELECT atribut2 FROM `itemset2` WHERE lolos = 1)');
-            $itemset12 = array_merge($item_set2_atribut1,$item_set2_atribut2);
-            $itemset3 = '';
-            $jumlahitemset2= count($itemset12);
-
-            if ($jumlahitemset2 > 0) {
-         
-                $varianset3 = [];
-
-                for ($s=0; $s < $jumlahitemset2 ; $s++) { 
-                    
-                    for ($t=0; $t < $jumlahitemset2 ; $t++) { 
-                        
-                        for ($u=0; $u < $jumlahitemset2 ; $u++) {
-
-                            if ($item_set1[$s]->atribut != $item_set1[$t]->atribut && $item_set1[$t]->atribut != $item_set1[$u]->atribut && $item_set1[$s]->atribut != $item_set1[$u]->atribut ) {
-                                
-                                $cekvarian1 = array('atribut1' => $itemset12[$s]->atribut,'atribut2' => $itemset12[$t]->atribut,'atribut3' => $itemset12[$u]->atribut);
-                                $cekvarian2 = array('atribut1' => $itemset12[$s]->atribut,'atribut2' => $itemset12[$u]->atribut,'atribut3' => $itemset12[$t]->atribut);
-                                $cekvarian3 = array('atribut1' => $itemset12[$t]->atribut,'atribut2' => $itemset12[$s]->atribut,'atribut3' => $itemset12[$u]->atribut);
-                                $cekvarian4 = array('atribut1' => $itemset12[$t]->atribut,'atribut2' => $itemset12[$u]->atribut,'atribut3' => $itemset12[$s]->atribut);
-                                $cekvarian5 = array('atribut1' => $itemset12[$u]->atribut,'atribut2' => $itemset12[$t]->atribut,'atribut3' => $itemset12[$s]->atribut);
-                                $cekvarian6 = array('atribut1' => $itemset12[$u]->atribut,'atribut2' => $itemset12[$s]->atribut,'atribut3' => $itemset12[$t]->atribut);
-                                
-
-
-                                if (!in_array($cekvarian1,$varian) && !in_array($cekvarian2,$varian) && !in_array($cekvarian3,$varian) && !in_array($cekvarian4,$varian) && !in_array($cekvarian5,$varian) && !in_array($cekvarian6,$varian)) {
-                                    
-                                    $varianset3[] = ['atribut1' => $itemset12[$s]->atribut,'atribut2' => $itemset12[$t]->atribut,'atribut3' => $itemset12[$u]->atribut];
-                                    
-                                }
-                                
-                            }
-                        }
-                    
-                    }
-                }
-
-                $jumlahvarianitemset3 = count($varianset3);
-                $itemsetjumlah3 = array();
-                $itemset3 = '';
-                
-                for ($j=0; $j < $jumlahvarianitemset3 ; $j++) { 
-                    // $jumlahtransaksi3 = DB::select('SELECT count(DISTINCT id_penjualan) as jumlah FROM penjualan_detail WHERE kode_produk = '.$varianset3[$j]['atribut1'] . '
-                    // AND id_penjualan IN(SELECT id_penjualan FROM penjualan_detail WHERE kode_produk = '.$varianset3[$j]['atribut2'].' AND id_penjualan IN(SELECT id_penjualan FROM penjualan_detail WHERE kode_produk = '.$varianset3[$j]['atribut3'].'))');
-                    // $itemsetjumlah3[] = ['atribut1' => $varianset3[$j]['atribut1'],'atribut2' => $varianset3[$j]['atribut2'],'atribut3'=>$varianset3[$j]['atribut3'],'jumlah' =>$jumlahtransaksi3[0]->jumlah,'support'=>$jumlahtransaksi3[0]->jumlah/$jumlah_transaksi*100,'lolos' => ($jumlahtransaksi3[0]->jumlah/$jumlah_transaksi*100 >= $min) ? 1:0
-                    // ];
-                    
-                    $varianset3a = $varianset3[$j]['atribut1'];
-                    $varianset3b = $varianset3[$j]['atribut2'];
-                    $varianset3c = $varianset3[$j]['atribut3'];
-
-                    $jumlahtransaksi3 = $this->hitung_set3($data_penjualan,$varianset3a,$varianset3b,$varianset3c);
-                    
-                    $itemsetjumlah3[] = ['atribut1' => $varianset3[$j]['atribut1'],'atribut2' => $varianset3[$j]['atribut2'],'atribut3'=>$varianset3[$j]['atribut3'],'jumlah' =>$jumlahtransaksi3,'support'=>$jumlahtransaksi3/$jumlah_penjualan*100,'lolos' => ($jumlahtransaksi3/$jumlah_penjualan*100 >= $min) ? 1:0
-                    ];
-                    
-                    
-
-                }
-
-                for ($j=0; $j < $jumlahvarianitemset3 ; $j++) { 
-                    $itemset3 .= '('. $itemsetjumlah3[$j]['atribut1'] .','.$itemsetjumlah3[$j]['atribut2'].','.$itemsetjumlah3[$j]['atribut3'].','.$itemsetjumlah3[$j]['jumlah'].','.$itemsetjumlah3[$j]['support'].','.$itemsetjumlah3[$j]['lolos'].'),';
-                }
-
-                $itemset3 = substr($itemset3, 0, -1);
-
-                $insertitemset2 = DB::select('INSERT INTO itemset3 (atribut1,atribut2,atribut3,jumlah,support,lolos) VALUES '.$itemset3);
-            
-
-            }
-
-        }        
-    }
-
     public function proses(){
 
-        $min = 25;
+        $min = 7;
+        $min_conf = 7;
 
         $data_mentah = DB::select('SELECT id_penjualan,GROUP_CONCAT(DISTINCT CONCAT(kode_produk)) AS produk FROM penjualan_detail GROUP BY id_penjualan ASC');
 
         $data_penjualan = array();
+        
         foreach ($data_mentah as $key => $value) {
             
             $produk = explode(",",$value->produk);
@@ -188,18 +29,17 @@ class ProsesAprioriController extends Controller{
         $clearitemset1= DB::select('TRUNCATE `itemset1`');
         $clearitemset2= DB::select('TRUNCATE `itemset2`');
         $clearitemset3= DB::select('TRUNCATE `itemset3`');
+        $clearconfidence = DB::select('TRUNCATE `confidence`');
     
-
         $item_set1 = DB::select('INSERT INTO `itemset1`(`atribut`, `jumlah`) SELECT kode_produk,count(DISTINCT id_penjualan) FROM `penjualan_detail` GROUP BY kode_produk');
 
-        $support_item1 = DB::select('UPDATE `itemset1` SET support =round( jumlah/'.$jumlah_penjualan .'*100),lolos = if(support > jumlah,1,0) ');
+        $support_item1 = DB::select('UPDATE `itemset1` SET support = jumlah/'.$jumlah_penjualan .'*100 ');
         $lolos_item1 = DB::select('UPDATE `itemset1` SET lolos = if(support >= '.$min.',1,0) ');
 
         $item_set1 = DB::select('SELECT atribut FROM `itemset1` WHERE lolos = 1');
         $jumlah_lolos_itemset1 = DB::select('SELECT count(atribut) as jumlah FROM `itemset1` WHERE lolos = 1');
 
-        if ($jumlah_lolos_itemset1 > 0) {
-            
+        if ($jumlah_lolos_itemset1 > 0) {        
         
             $varian = array();
 
@@ -235,7 +75,7 @@ class ProsesAprioriController extends Controller{
                 $jumlahtransaksi2 = $this->hitung_set2($data_mentah,$varianset2a,$varianset2b);
                 
                 
-                $itemsetjumlah[] = ['atribut1' => $varian[$j]['atribut1'],'atribut2' => $varian[$j]['atribut2'],'jumlah'=>$jumlahtransaksi2,'support'=>round($jumlahtransaksi2/$jumlah_penjualan * 100), 'lolos' => (round($jumlahtransaksi2/$jumlah_penjualan * 100) >= $min)?1:0];
+                $itemsetjumlah[] = ['atribut1' => $varian[$j]['atribut1'],'atribut2' => $varian[$j]['atribut2'],'jumlah'=>$jumlahtransaksi2,'support'=>$jumlahtransaksi2/$jumlah_penjualan * 100, 'lolos' => ($jumlahtransaksi2/$jumlah_penjualan * 100 >= $min)?1:0];
                         
             }
 
@@ -301,7 +141,7 @@ class ProsesAprioriController extends Controller{
 
                     $jumlahtransaksi3 = $this->hitung_set3($data_mentah,$varianset3a,$varianset3b,$varianset3c);
                     
-                    $itemsetjumlah3[] = ['atribut1' => $varianset3[$j]['atribut1'],'atribut2' => $varianset3[$j]['atribut2'],'atribut3'=>$varianset3[$j]['atribut3'],'jumlah' =>$jumlahtransaksi3,'support'=>round($jumlahtransaksi3/$jumlah_penjualan*100),'lolos' => (round($jumlahtransaksi3/$jumlah_penjualan*100) >= $min) ? 1:0
+                    $itemsetjumlah3[] = ['atribut1' => $varianset3[$j]['atribut1'],'atribut2' => $varianset3[$j]['atribut2'],'atribut3'=>$varianset3[$j]['atribut3'],'jumlah' =>$jumlahtransaksi3,'support'=>$jumlahtransaksi3/$jumlah_penjualan*100,'lolos' => ($jumlahtransaksi3/$jumlah_penjualan*100 >= $min) ? 1:0
                     ];
                             
                 }
@@ -317,11 +157,62 @@ class ProsesAprioriController extends Controller{
 
             }
             
+            $jumlah_lolos_itemset3 = DB::select('SELECT count(lolos) AS jumlah FROM `itemset3` WHERE lolos = 1');
+
+            if ($jumlah_lolos_itemset3[0]->jumlah > 0) {
+                
+                $data_itemset3 = DB::select('SELECT * FROM `itemset3` WHERE lolos = 1');
+
+                $hitung_confidence = array();
+
+                foreach ($data_itemset3 as $key => $value) {
+                    
+                    $atribut1 = $value->atribut1;
+                    $atribut2 = $value->atribut2;
+                    $atribut3 = $value->atribut3;
+                    $support_xuy = $value->support;
+
+                    // a,b => c
+                    $hitung_confidence1 = $this->hitung_confidence($data_mentah,$atribut1,$atribut2,$atribut3,$support_xuy,$min_conf,$jumlah_penjualan,$min);
         
-        
+                    $hitung_confidence2 = $this->hitung_confidence($data_mentah,$atribut2,$atribut3,$atribut1,$support_xuy,$min_conf,$jumlah_penjualan,$min);
+                    
+                    $hitung_confidence3 = $this->hitung_confidence($data_mentah,$atribut3,$atribut1,$atribut2,$support_xuy,$min_conf,$jumlah_penjualan,$min);
+
+                    array_push($hitung_confidence,$hitung_confidence1,$hitung_confidence2,$hitung_confidence3) ;
+                }
+                $itemconfidence = '';
+                $jumlahconfidence = count($hitung_confidence);
+                for ($j=0; $j < $jumlahconfidence ; $j++) { 
+                    $itemconfidence .= '('. 
+                    $hitung_confidence[$j]['kombinasi1'] .','.$hitung_confidence[$j]['kombinasi2'].','.$hitung_confidence[$j]['kombinasi3'].','.$hitung_confidence[$j]['support_xUy'].','.
+                    $hitung_confidence[$j]['support_x'].','.
+                    $hitung_confidence[$j]['confidence'] .','.
+                    $hitung_confidence[$j]['lolos'] .','.
+                    $hitung_confidence[$j]['min_support'] .','.
+                    $hitung_confidence[$j]['min_confidence'] .','.
+                    $hitung_confidence[$j]['nilai_uji_lift'] .',"'.
+                    $hitung_confidence[$j]['korelasi_rule'] .'",'.
+                    $hitung_confidence[$j]['id_process'] .','.
+                    $hitung_confidence[$j]['jumlah_a'] .','.
+                    $hitung_confidence[$j]['jumlah_b'] .','.
+                    $hitung_confidence[$j]['jumlah_ab'] .','.
+                    $hitung_confidence[$j]['px'] .','.
+                    $hitung_confidence[$j]['py'] .','.
+                    $hitung_confidence[$j]['pxuy'] .','.
+                    $hitung_confidence[$j]['from_itemset'] .'),';
+                }
+
+                $itemconfidence = substr($itemconfidence, 0, -1);
+                
+                $insertconfidence = DB::select('INSERT INTO confidence (`kombinasi1`, `kombinasi2`, `kombinasi3`, `support_xUy`, `support_x`, `confidence`, `lolos`, `min_support`, `min_confidence`, `nilai_uji_lift`, `korelasi_rule`, `id_process`, `jumlah_a`, `jumlah_b`, `jumlah_ab`, `px`, `py`, `pxuy`, `from_itemset`)  VALUES '.$itemconfidence);
+            }
+
         }
 
     }
+
+
 
     public function hitung_set2($data_mentah,$varianset2a,$varianset2b){
         // dd($varianset2a);
@@ -372,6 +263,64 @@ class ProsesAprioriController extends Controller{
 
         return $jumlahset3;
 
+    }
+
+
+    public function hitung_confidence($data_mentah,$atribut1,$atribut2,$atribut3,$support_xuy,$min_conf,$jumlah_penjualan,$min){
+
+        $jumlah_itemset_1_kombinasi = DB::select('SELECT jumlah FROM `itemset1` WHERE atribut = '.$atribut3);
+        $jumlah_itemset_2_kombinasi = $this->hitung_set2($data_mentah,$atribut1,$atribut2);        
+        $jumlah_itemset_3_kombinasi = $this->hitung_set3($data_mentah,$atribut1,$atribut2,$atribut3);
+
+        $jumlah_kemunculanA = $jumlah_itemset_2_kombinasi;
+        $jumlah_kemunculanB = $jumlah_itemset_1_kombinasi[0]->jumlah;
+        $jumlah_kemunculanAB = $jumlah_itemset_3_kombinasi;
+
+        $nilai_suport_itemset_2_kombinasi = $jumlah_itemset_2_kombinasi/$jumlah_penjualan*100;
+
+        $confidence = $support_xuy/$nilai_suport_itemset_2_kombinasi*100;
+        
+        $lolos = ($confidence >= $min_conf) ? 1:0;
+        
+        $paUb = $jumlah_kemunculanAB/$jumlah_penjualan;
+
+        
+        $nilai_uji_lift = $paUb/(($jumlah_kemunculanA/$jumlah_penjualan)*($jumlah_kemunculanB/$jumlah_penjualan));
+        
+        switch (true) {
+            case $nilai_uji_lift < 1:
+                $korelasi_rule = "korelasi negatif";
+                break;
+            
+            case $nilai_uji_lift > 1:
+                $korelasi_rule = "korelasi positif";
+                break;
+            default:
+                $korelasi_rule = "tidak ada korelasi";
+                break;
+        }
+
+        return $data = [
+            "kombinasi1" => $atribut1,
+            "kombinasi2" => $atribut2,
+            "kombinasi3" => $atribut3,
+            "support_xUy" => $support_xuy,
+            "support_x" => $nilai_suport_itemset_2_kombinasi,
+            "confidence" => $confidence,
+            "lolos" => $lolos,
+            "min_support" => $min,
+            "min_confidence" => $min_conf,
+            "nilai_uji_lift" => $nilai_uji_lift,
+            "korelasi_rule" => $korelasi_rule,
+            "id_process" => 1,
+            "jumlah_a" => $jumlah_kemunculanA,
+            "jumlah_b" => $jumlah_kemunculanB,
+            "jumlah_ab" => $jumlah_kemunculanAB,
+            "px" => ($jumlah_kemunculanA/$jumlah_penjualan),
+            "py" => ($jumlah_kemunculanB/$jumlah_penjualan),
+            "pxuy" => $paUb,
+            "from_itemset"=>3
+        ];        
 
     }
 }
